2046b489a6c417a19795b5c35ee221b30ac2578e
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-11-04 09:06:45 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <div>
+            <h2> Você está na home </h2>
+        </div>
+    )
+}
+export default Home
---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+function Login() {
+    return (
+        <div>
+            <h2> Você está na Login </h2>
+        </div>
+    )
+}
+export default Login
